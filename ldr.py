91b248ae82c4bb20b3172c2c5a89e9e636b31ae3
Razvan <razvanlevarda@gmail.com>
2022-10-30 00:45:36 +0300

    Update ldr.py

--- ldr.py
+++ ldr.py
@@ -1,9 +1,16 @@
 import machine
 import utime
  
-analog_value = machine.ADC(26)
- 
+analog_value1 = machine.ADC(26)
+analog_value2 = machine.ADC(27)
+analog_value3 = machine.ADC(28)
+
 while True:
-    reading = analog_value.read_u16()     
-    print("ADC: ",reading)
+    reading1 = analog_value1.read_u16()
+    reading2 = analog_value2.read_u16()
+    reading3 = analog_value3.read_u16()
+    total = (reading1+reading2+reading3)
+    if total < 185000:
+        print("Baaang!")
+        print("ADC: ",total )
     utime.sleep(0.2)
